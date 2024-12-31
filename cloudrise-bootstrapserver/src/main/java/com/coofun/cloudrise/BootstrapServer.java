2be71dba09911bf5503176e0ff41bb24a5390646
coofun <coofun123@gmail.com>
2025-01-01 04:56:17 +0800

    init project

---
+++ cloudrise-bootstrapserver/src/main/java/com/coofun/cloudrise/BootstrapServer.java
@@ -0,0 +1,11 @@
+package com.coofun.cloudrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapServer {
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapServer.class, args);
+    }
+}
---
+++ cloudrise-bootstrapserver/src/test/java/com/coofun/cloudrise/BootstrapServerTests.java
@@ -0,0 +1,11 @@
+package com.coofun.cloudrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BootstrapServerTests {
+    @Test
+    void contextLoads() {
+    }
+}
